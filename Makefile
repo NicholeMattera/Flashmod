243f523f0e3d84a38f77f317cdaea1292757fb7c
Nichole Mattera <me@nicholemattera.gay>
2020-10-10 06:38:19 -0400

    version 1.5

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
 TARGET = flashmod
-OBJS = main.o 
+OBJS = main.o graphics.o framebuffer.o
 
 CFLAGS = -O2 -G0 -Wall
 CXXFLAGS = $(CFLAGS) -fno-exceptions -fno-rtti
 ASFLAGS = $(CFLAGS)
 
+LIBDIR =
+LIBS = -lpspgu -lpng -lz -lm
+LDFLAGS =
+
 EXTRA_TARGETS = EBOOT.PBP
 PSP_EBOOT_ICON = ICON0.png
-PSP_EBOOT_TITLE = FlashMod Version 1.0
+PSP_EBOOT_TITLE = FlashMod Version 1.5
 
 PSPSDK=$(shell psp-config --pspsdk-path)
 include $(PSPSDK)/lib/build.mak
--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include <pspdisplay.h>
 #include <pspctrl.h>
 #include <pspiofilemgr.h>
+#include <pspgu.h>
+#include <png.h>
 #include <stdlib.h>
 #include <string.h>
-
-PSP_MODULE_INFO("FlashMod", 0, 1, 1);
-PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
+#include "graphics.h"
 
 #define printf	pspDebugScreenPrintf
-char write_buffer[128*1024];
+#define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
+
+PSP_MODULE_INFO("FlashMod", 0, 1, 1);
+
 
 int exit_callback(int arg1, int arg2, void *common) {
 	sceKernelExitGame();
@@ -35,22 +38,18 @@ int SetupCallbacks(void) {
 	return thid;
 }
 
+char write_buffer[128*1024];
+
 void write_file(const char *readpath, const char *writepath)
 {
 	int fdin;
 	int fdout;
 
-	printf("Writing %s\n", writepath);
 	fdin = sceIoOpen(readpath, PSP_O_RDONLY, 0777);
 	if(fdin >= 0)
 	{
 		int bytesRead = 1;
 		fdout = sceIoOpen(writepath, PSP_O_WRONLY | PSP_O_CREAT | PSP_O_TRUNC, 0777);
-		if(fdout < 0)
-		{
-			printf("Couldn't open %s\n", writepath);
-		}
-
 		bytesRead = sceIoRead(fdin, write_buffer, sizeof(write_buffer));
 		while((bytesRead > 0) && (fdout >= 0))
 		{
@@ -68,18 +67,27 @@ void write_file(const char *readpath, const char *writepath)
 			sceIoClose(fdin);
 		}
 	}
-	else
-	{
-		printf("Couldn't open %s\n", readpath);
-	}
 }
 
 
 int main() {
-	SceCtrlData pad;
-	
+	int i = 0; SceCtrlData pad;
+	int menu = 1;
+        char menu_one_buffer[200];
+        char menu_two_buffer[200];
+        char menu_three_buffer[200];
+        char menu_four_buffer[200];
+        char menu_five_buffer[200];
+	char flash_complete_buffer[200];
+        Image* menu_one;	
+        Image* menu_two;
+        Image* menu_three;
+        Image* menu_four;
+        Image* menu_five;
+	Image* flash_complete;
+
 	SetupCallbacks();
-	pspDebugScreenInit();
+	initGraphics();
 
 	sceCtrlSetSamplingCycle(0);
 	sceCtrlSetSamplingMode(PSP_CTRL_MODE_DIGITAL);
@@ -87,56 +95,247 @@ int main() {
 	sceIoUnassign("flash0:"); 
 	sceIoAssign("flash0:", "lflash0:0,0", "flashfat0:", 0, NULL, 0);
 
-	pspDebugScreenSetTextColor(0xFFFFFF);
-	printf ("FlashMod - by Orbis PSP Development\n");
-	printf ("-----------------------------------\n");
-	printf ("Cross - Add Asura Effect\n");
-	printf ("Circle - Remove Asura Effect\n");
-	printf ("Triangle - Add Backgrounds\n");
-	printf ("Square - Restore Defaults Backgrounds\n");
+	sprintf(menu_one_buffer,"menu1.png");
+	menu_one = loadImage(menu_one_buffer);
+	sprintf(menu_two_buffer,"menu2.png");
+	menu_two = loadImage(menu_two_buffer);
+	sprintf(menu_three_buffer,"menu3.png");
+	menu_three = loadImage(menu_three_buffer);
+	sprintf(menu_four_buffer,"menu4.png");
+	menu_four = loadImage(menu_four_buffer);
+	sprintf(menu_five_buffer,"menu5.png");
+	menu_five = loadImage(menu_five_buffer);
+	sprintf(flash_complete_buffer,"complete.png");
+	flash_complete = loadImage(flash_complete_buffer);
 
+	int x = 0;
+        int y = 0;
+        sceDisplayWaitVblankStart();
+
+	while (x < 480) {
+		while (y < 272) {
+			blitAlphaImageToScreen(0 ,0 ,480 , 272, menu_one, x, y);
+			y += 272;
+		}
+		x += 480;
+		y = 0;
+	}
+	flipScreen();
+		
 	while (1) {
 		sceCtrlReadBufferPositive(&pad, 1);
+
+		if (menu == 1) {
+			x = 0;
+    			y = 0;
+      			sceDisplayWaitVblankStart();
+
+			while (x < 480) {
+				while (y < 272) {
+					blitAlphaImageToScreen(0 ,0 ,480 , 272, menu_one, x, y);
+					y += 272;
+				}
+				x += 480;
+				y = 0;
+			}
+			flipScreen();
+		}
 		
+		if (menu == 2) {
+			x = 0;
+    			y = 0;
+      			sceDisplayWaitVblankStart();
+	
+			while (x < 480) {
+				while (y < 272) {
+					blitAlphaImageToScreen(0 ,0 ,480 , 272, menu_two, x, y);
+					y += 272;
+				}
+				x += 480;
+				y = 0;
+			}
+			flipScreen();
+		}
+
+		if (menu == 3) {
+			x = 0;
+    			y = 0;
+      			sceDisplayWaitVblankStart();
+
+			while (x < 480) {
+				while (y < 272) {
+					blitAlphaImageToScreen(0 ,0 ,480 , 272, menu_three, x, y);
+					y += 272;
+				}
+				x += 480;
+				y = 0;
+			}
+			flipScreen();
+		}
+		
+		if (menu == 4) {
+			x = 0;
+    			y = 0;
+      			sceDisplayWaitVblankStart();
+	
+			while (x < 480) {
+				while (y < 272) {
+					blitAlphaImageToScreen(0 ,0 ,480 , 272, menu_four, x, y);
+					y += 272;
+				}
+				x += 480;
+				y = 0;
+			}
+			flipScreen();
+		}
+
+		if (menu == 5) {
+			x = 0;
+    			y = 0;
+      			sceDisplayWaitVblankStart();
+	
+			while (x < 480) {
+				while (y < 272) {
+					blitAlphaImageToScreen(0 ,0 ,480 , 272, menu_five, x, y);
+					y += 272;
+				}
+				x += 480;
+				y = 0;
+			}
+			flipScreen();
+		}
+		
+		if (pad.Buttons & PSP_CTRL_UP) {
+			if (menu > 1) {
+				menu--;
+			        for(i=0; i<5; i++) {
+		                	sceDisplayWaitVblankStart();
+				}
+          		}
+		}
+
+		if (pad.Buttons & PSP_CTRL_DOWN) {
+			if (menu < 5) {
+				menu++;
+			        for(i=0; i<5; i++) {
+		                	sceDisplayWaitVblankStart();
+				}
+			}
+		}
+
 		if (pad.Buttons & PSP_CTRL_CROSS) {
-			write_file("ms0:/PSP/GAME/FlashMod/ASURA_EFFECT_ON/system_plugin_bg.rco","flash0:/vsh/resource/system_plugin_bg.rco");
+			if (menu == 1) {
+				write_file("./ASURA_EFFECT_OFF/system_plugin_bg.rco","flash0:/vsh/resource/system_plugin_bg.rco");
+
+				x = 0;
+    				y = 0;
+	      			sceDisplayWaitVblankStart();
+
+				while (x < 480) {
+					while (y < 272) {
+						blitAlphaImageToScreen(0 ,0 ,480 , 272, flash_complete, x, y);
+						y += 272;
+					}
+					x += 480;
+					y = 0;
+				}
+				flipScreen();
+
+			        for(i=0; i<30; i++) {
+		                	sceDisplayWaitVblankStart();
+				}
+			}
+			if (menu == 2) {
+				write_file("./ASURA_EFFECT_ON/system_plugin_bg.rco","flash0:/vsh/resource/system_plugin_bg.rco");
+
+				x = 0;
+    				y = 0;
+	      			sceDisplayWaitVblankStart();
+
+				while (x < 480) {
+					while (y < 272) {
+						blitAlphaImageToScreen(0 ,0 ,480 , 272, flash_complete, x, y);
+						y += 272;
+					}
+					x += 480;
+					y = 0;
+				}
+				flipScreen();
+
+			        for(i=0; i<30; i++) {
+		                	sceDisplayWaitVblankStart();
+				}
+			}
+			if (menu == 3) {
+				write_file("ms0:/01.bmp","flash0:/vsh/resource/01.bmp");
+				write_file("ms0:/02.bmp","flash0:/vsh/resource/02.bmp");
+				write_file("ms0:/03.bmp","flash0:/vsh/resource/03.bmp");
+				write_file("ms0:/04.bmp","flash0:/vsh/resource/04.bmp");
+				write_file("ms0:/05.bmp","flash0:/vsh/resource/05.bmp");
+				write_file("ms0:/06.bmp","flash0:/vsh/resource/06.bmp");
+				write_file("ms0:/07.bmp","flash0:/vsh/resource/07.bmp");
+				write_file("ms0:/08.bmp","flash0:/vsh/resource/08.bmp");
+				write_file("ms0:/09.bmp","flash0:/vsh/resource/09.bmp");
+				write_file("ms0:/10.bmp","flash0:/vsh/resource/10.bmp");
+				write_file("ms0:/11.bmp","flash0:/vsh/resource/11.bmp");
+				write_file("ms0:/12.bmp","flash0:/vsh/resource/12.bmp");
+
+				x = 0;
+    				y = 0;
+	      			sceDisplayWaitVblankStart();
+
+				while (x < 480) {
+					while (y < 272) {
+						blitAlphaImageToScreen(0 ,0 ,480 , 272, flash_complete, x, y);
+						y += 272;
+					}
+					x += 480;
+					y = 0;
+				}
+				flipScreen();
+
+			        for(i=0; i<30; i++) {
+		                	sceDisplayWaitVblankStart();
+				}
+			}
+			if (menu == 4) {
+				write_file("./DEFAULT_BG/01.bmp","flash0:/vsh/resource/01.bmp");
+				write_file("./DEFAULT_BG/02.bmp","flash0:/vsh/resource/02.bmp");
+				write_file("./DEFAULT_BG/03.bmp","flash0:/vsh/resource/03.bmp");
+				write_file("./DEFAULT_BG/04.bmp","flash0:/vsh/resource/04.bmp");
+				write_file("./DEFAULT_BG/05.bmp","flash0:/vsh/resource/05.bmp");
+				write_file("./DEFAULT_BG/06.bmp","flash0:/vsh/resource/06.bmp");
+				write_file("./DEFAULT_BG/07.bmp","flash0:/vsh/resource/07.bmp");
+				write_file("./DEFAULT_BG/08.bmp","flash0:/vsh/resource/08.bmp");
+				write_file("./DEFAULT_BG/09.bmp","flash0:/vsh/resource/09.bmp");
+				write_file("./DEFAULT_BG/10.bmp","flash0:/vsh/resource/10.bmp");
+				write_file("./DEFAULT_BG/11.bmp","flash0:/vsh/resource/11.bmp");
+				write_file("./DEFAULT_BG/12.bmp","flash0:/vsh/resource/12.bmp");
+
+				x = 0;
+    				y = 0;
+	      			sceDisplayWaitVblankStart();
+
+				while (x < 480) {
+					while (y < 272) {
+						blitAlphaImageToScreen(0 ,0 ,480 , 272, flash_complete, x, y);
+						y += 272;
+					}
+					x += 480;
+					y = 0;
+				}
+				flipScreen();
+
+			        for(i=0; i<30; i++) {
+		                	sceDisplayWaitVblankStart();
+				}
+			}
+			if (menu == 5) {
+				break;
+			}
 		}	
-
-		if (pad.Buttons & PSP_CTRL_CIRCLE) {
-			write_file("ms0:/PSP/GAME/FlashMod/ASURA_EFFECT_OFF/system_plugin_bg.rco","flash0:/vsh/resource/system_plugin_bg.rco");
-		}			
-
-		if (pad.Buttons & PSP_CTRL_TRIANGLE) {
-			write_file("ms0:/01.bmp","flash0:/vsh/resource/01.bmp");
-			write_file("ms0:/02.bmp","flash0:/vsh/resource/02.bmp");
-			write_file("ms0:/03.bmp","flash0:/vsh/resource/03.bmp");
-			write_file("ms0:/04.bmp","flash0:/vsh/resource/04.bmp");
-			write_file("ms0:/05.bmp","flash0:/vsh/resource/05.bmp");
-			write_file("ms0:/06.bmp","flash0:/vsh/resource/06.bmp");
-			write_file("ms0:/07.bmp","flash0:/vsh/resource/07.bmp");
-			write_file("ms0:/08.bmp","flash0:/vsh/resource/08.bmp");
-			write_file("ms0:/09.bmp","flash0:/vsh/resource/09.bmp");
-			write_file("ms0:/10.bmp","flash0:/vsh/resource/10.bmp");
-			write_file("ms0:/11.bmp","flash0:/vsh/resource/11.bmp");
-			write_file("ms0:/12.bmp","flash0:/vsh/resource/12.bmp");
-		}			
-
-		if (pad.Buttons & PSP_CTRL_SQUARE) {
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/01.bmp","flash0:/vsh/resource/01.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/02.bmp","flash0:/vsh/resource/02.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/03.bmp","flash0:/vsh/resource/03.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/04.bmp","flash0:/vsh/resource/04.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/05.bmp","flash0:/vsh/resource/05.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/06.bmp","flash0:/vsh/resource/06.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/07.bmp","flash0:/vsh/resource/07.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/08.bmp","flash0:/vsh/resource/08.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/09.bmp","flash0:/vsh/resource/09.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/10.bmp","flash0:/vsh/resource/10.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/11.bmp","flash0:/vsh/resource/11.bmp");
-			write_file("ms0:/PSP/GAME/FlashMod/DEFAULT_BG/12.bmp","flash0:/vsh/resource/12.bmp");
-		}			
-
 	}
+	sceKernelExitGame();
 	return 0;
 }
 		
